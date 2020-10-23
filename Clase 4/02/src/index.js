c9acfd862219b0a525e21a6378055f0e43397a05
Ezequiel Yacar <ezequielyacar@Air-de-Ezequiel.fibertel.com.ar>
2020-10-23 20:57:38 -0300

    commit

--- Clase 4/02/src/index.js
+++ Clase 4/02/src/index.js
@@ -19,9 +19,9 @@ useEffect(()=>{
 GetLista();
 // eslint-disable-next-line react-hooks/exhaustive-deps 
 },[])
-console.log(search.length)
+
 const p = search.length
-console.log(p)
+
 return(
 <>
 <input type="search" name="" id="" placeholder='Search...' onChange={Buscando}/>
@@ -46,14 +46,17 @@ const Libro = () => {
   GetLibro();
   // eslint-disable-next-line react-hooks/exhaustive-deps 
   },[])
-console.log(params.id)
-console.log(Datos)
 
-return (<><h2><u>Elegido</u></h2>
+
+return (
+<div id="elegido">
+<h2><u>Elegido</u></h2>
 {Datos?
-Datos.filter(libro => libro.isbn===params.id).map((libro,i)=><ul><li>Titulo: {libro.title}</li><li>Codigo del libro: {libro.isbn}</li><li>Cantidad de paginas: {libro.pageCount}</li><li>Tapa: <br/> <img src={libro.thumbnailUrl} alt="Tapa"/></li><li>Descripción: {libro.longDescription}</li><li>Autor: {libro.authors}</li><li>Categoria: {libro.categories}</li></ul>)
+Datos.filter(libro => libro.isbn===params.id).map((libro,i)=><ul><li><span className='titulos'>Titulo: </span>{libro.title}</li><li><span className='titulos'>Codigo del libro: </span>{libro.isbn}</li><li><span className='titulos'>Cantidad de paginas: </span>{libro.pageCount}</li><li><span className='titulos'>Tapa: </span><br/><br/> <img src={libro.thumbnailUrl} alt="Tapa"/></li><li><span className='titulos'>Descripción: </span>{libro.longDescription}</li><li><span className='titulos'>Autor: </span>{libro.authors}</li><li><span className='titulos'>Categoria: </span>{libro.categories}</li></ul>)
 :<img src="https://images.vectorhq.com/images/previews/8da/preload-gif-animated-freebies-83609.gif" alt=""/>}
-<Link to='/'>Regresa a la Home</Link></>)
+
+<Link to='/' id='ancor'><input type="button" value="Regresa a la Home"/></Link>
+</div>)
 };
 
 
@@ -61,7 +64,7 @@ const App = () =>{
 
 return(
 <Router>
-<h1>Libros:</h1>
+<h1><u><em>Buscador de libros</em></u></h1>
 <Switch>
 <Route exact path="/"><Libros/></Route>
 <Route exact path="/pais/:id"><Libro/></Route>
